1ff8fb85761698fab56266ff0bafc1e510d119c3
thejokeson-you <milansaleh2004@gmail.com>
2025-06-28 15:24:50 +0100

    Add methods

--- python_implementation/main.py
+++ python_implementation/main.py
@@ -9,3 +9,14 @@ while i < 500:
   Z.append(i)
   i += 1
 
+# Generate CRS string from trusted third party, assuming TTP exists
+def generate_crs():
+
+
+def setup():
+
+
+def prove():
+
+
+def verify():
