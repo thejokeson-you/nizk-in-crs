92c6cff587a059af0d222f1d32a18eb5b5b62b48
thejokeson-you <milansaleh2004@gmail.com>
2025-07-03 15:36:44 +0100

    Generate large prime

--- python_implementation/main.py
+++ python_implementation/main.py
@@ -1,9 +1,20 @@
 # main.py
 
 import numpy as np
+import hashlib
+import random
 
-Z = []
 i = 0
+
+# 2048-bit prime, for standard security
+p_hex = """
+FFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD1
+29024E088A67CC74020BBEA63B139B22514A08798E3404DD
+EF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245
+E485B576625E7EC6F44C42E9A63A3620FFFFFFFFFFFFFFFF
+""".replace("\n", "").replace(" ", "")
+
+p = int(p_hex, 16)
 
 while i < 500:
   Z.append(i)
