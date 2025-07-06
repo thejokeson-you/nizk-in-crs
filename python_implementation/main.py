69a43840db2a968a2224cf39ca6674559b79a46e
thejokeson-you <milansaleh2004@gmail.com>
2025-07-06 12:36:20 +0100

    Add variables

--- python_implementation/main.py
+++ python_implementation/main.py
@@ -16,17 +16,20 @@ class Group:
         return pow(base, exponent, self.p)
 
 
-# 2048-bit prime, for standard security against DLog attacks
-p_hex = """
-FFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD1
-29024E088A67CC74020BBEA63B139B22514A08798E3404DD
-EF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245
-E485B576625E7EC6F44C42E9A63A3620FFFFFFFFFFFFFFFF
-""".replace("\n", "").replace(" ", "")
+# 2048-bit prime, for standard security against DLog attacks. Included for demonstration purposes
+# p_hex = """
+# FFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD1
+# 29024E088A67CC74020BBEA63B139B22514A08798E3404DD
+# EF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245
+# E485B576625E7EC6F44C42E9A63A3620FFFFFFFFFFFFFFFF
+# """.replace("\n", "").replace(" ", "")
 
-p = int(p_hex, 16)
+# p = int(p_hex, 16)
+
+# 2048-bit prime number without converting first. 
+p = 208351617316091241234326746312124448251235562226470491514186331217050270460481 
+
 g = 2
-# q = (p-1) // 2
 
 # Use Fiat-Shamir to hash given elements
 def fiat_shamir_hash(*elements):
